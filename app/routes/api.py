24b9a79e620ac0155c2fe7d227623a8078d36b7b
Harborth, Allen <allenharborth9835@gmail.com>
2022-02-26 19:41:23 -0600

    add session functionality

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,13 +10,24 @@ def signup():
   data = request.get_json()
   db = get_db()
   
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+  try:
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  db.add(newUser)
-  db.commit()
+    db.add(newUser)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
 
+    db.rollback()
+    return jsonify(message = 'signup failed'), 500
+
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
+  
   return jsonify(id = newUser.id)
+
